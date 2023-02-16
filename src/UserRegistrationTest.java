5ad675da4d5fd334889ac566927e3440f029f128
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-02-16 22:29:26 +0530

    added: program to check Password First Rule using JUNIT test cases

---
+++ src/UserRegistrationTest.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.userRegistrationTest;
+
+import org.junit.*;
+import junit.framework.Assert;
+
+public class UserRegistrationTest {
+    @Before
+    public void welcomeMsg() {
+        System.out.println("Welcome to User Registration Problem");
+    }
+
+    @After
+    public void afterTest() {
+
+        System.out.println("after every test case");
+    }
+
+    @Test
+    public void checkFirstName() {
+        UserRegistration validation = new UserRegistration();
+        Assert.assertEquals(true, validation.checkFirstName("Sreedhar"));
+    }
+
+    @Test
+    public void checkLastName() {
+        UserRegistration validation = new UserRegistration();
+        Assert.assertEquals(true, validation.checkLastName("Nagalli"));
+    }
+
+    @Test
+    public void checkEmail() {
+        UserRegistration validation = new UserRegistration();
+        Assert.assertEquals(true, validation.checkEmail("nsridhary2k@gmail.com"));
+    }
+
+    @Test
+    public void checkMobileNumber() {
+        UserRegistration validation = new UserRegistration();
+        Assert.assertEquals(true, validation.checkMobileNumber("91 6746847843"));
+    }
+
+    @Test
+    public void checkPassword() {
+        UserRegistration validation = new UserRegistration();
+        Assert.assertEquals(true, validation.checkPassword("Sri19189"));
+    }
+}
--- src/com/bridgelabz/userRegistrationTest/UserRegistration.java
+++ src/com/bridgelabz/userRegistrationTest/UserRegistration.java
@@ -7,22 +7,25 @@ public class UserRegistration {
     public static final String VALIDATE_NAME = "^[A-Z]+[a-z]{2,}$";
     public static final String EMAIL_ID = "^[a-z]{3,}[.][a-z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.][a-z]{2,}$";
     public static final String PHONE_NUMBER = "^[91]+[ ][6-9]{1}[0-9]{9}$";
-    
-    
+    public static final String PASSWORD_FIRSTRULE = "[A-Za-z0-9]{8,}$";
 
     public boolean checkFirstName(String firstName) {
         return (Pattern.matches(VALIDATE_NAME, firstName));
     }
-    
+
     public boolean checkLastName(String lastName) {
         return (Pattern.matches(VALIDATE_NAME, lastName));
     }
-    
+
     public boolean checkEmail(String email) {
         return (Pattern.matches(EMAIL_ID, email));
     }
-    
+
     public boolean checkMobileNumber(String phoneNumber) {
         return (Pattern.matches(PHONE_NUMBER, phoneNumber));
+    }
+
+    public boolean checkPassword(String PASSWORD) {
+        return (Pattern.matches(PASSWORD_FIRSTRULE, PASSWORD));
     }
 }
